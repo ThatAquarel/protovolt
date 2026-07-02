aedc2e6341ef1458fc13b04ae29777a1358b02b1
ThatAquarel <xia_tianyi@outlook.com>
2026-07-02 09:37:03 -0400

    bump to v1.4.2: load protov-hal from protov-bootloader

--- protov-hal/build.rs
+++ protov-hal/build.rs
@@ -1,36 +1,27 @@
-//! This build script copies the `memory.x` file from the crate root into
-//! a directory where the linker can always find it at build time.
-//! For many projects this is optional, as the linker always searches the
-//! project root directory -- wherever `Cargo.toml` is. However, if you
-//! are using a workspace or have a more complicated build setup, this
-//! build script becomes required. Additionally, by requesting that
-//! Cargo re-run the build script whenever `memory.x` is changed,
-//! updating `memory.x` ensures a rebuild of the application with the
-//! new memory settings.
+//! Linker script from protov-nvm (application profile).
 
 use std::env;
-use std::fs::File;
-use std::io::Write;
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-    // Put `memory.x` in our output directory and ensure it's
-    // on the linker search path.
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
+    fs::write(
+        out.join("memory.x"),
+        include_str!("../protov-nvm/linker/memory-application.x"),
+    )
+    .unwrap();
+
+    println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed=../protov-nvm/linker/memory-application.x");
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
-    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
+
+    // With protov-bootloader, remove link-rp.x
+    // such that .boot2 loads to proper address
+    // arm-none-eabi-objdump -h target/thumbv6m-none-eabi/release/protov | grep .boot2
+    // println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
 }
